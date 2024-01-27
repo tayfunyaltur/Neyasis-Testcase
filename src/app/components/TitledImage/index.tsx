bcb2978e3a6bc357718a1f3aba5d627a34529fd8
Tayfun Yaltur <tayfunyaltur@Tayfun-MacBook-Air.local>
2024-01-27 18:42:36 +0300

    titled images implemented

---
+++ src/app/components/TitledImage/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import classnames from "./titled-image.module.css";
+
+interface TitledImageProps {
+    titles: string[];
+    image: string;
+}
+
+const TitledImage = ({ image, titles }: TitledImageProps) => {
+    return (
+        <div className={classnames.container}>
+            <img src={image} />
+            <div className={classnames.titleContainer}>
+                {titles.map((title, index) => (
+                    <>
+                        <a href="#" key={index} className={classnames.title}>
+                            {title}
+                        </a>
+                        {titles.length > 1 && index < titles.length - 1 && (
+                            <div className={classnames.divider} />
+                        )}
+                    </>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default TitledImage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,9 @@
 import Carousel from "./components/Carousel";
 import SearchSlider from "./components/SearchSlider";
 import Search from "./components/Searh";
+import TitledImage from "./components/TitledImage";
+import { SliderItems, openPositions } from "./constants";
 import classnames from "./page.module.css";
-
-const SliderItems = [
-    "UX Designer",
-    "UI Designer",
-    "Front-end Developer",
-    "Back-end Developer",
-    "IOS Developer",
-];
-
-const openPositions = [
-    {
-        position: "IT Müdürü",
-        company: {
-            name: "Bosch",
-            logo: "bosch.png",
-        },
-    },
-    {
-        position: "Front-end Developer",
-        company: {
-            name: "Aktif Bank",
-            logo: "aktifbank.jpg",
-        },
-    },
-    {
-        position: "Back-end Developer",
-        company: {
-            name: "Akbank",
-            logo: "akbank.webp",
-        },
-    },
-    {
-        position: ".Net Developer",
-        company: {
-            name: "Arçelik",
-            logo: "arc-elik.png",
-        },
-    },
-    {
-        position: "Junior .Net Developer",
-        company: {
-            name: "Allianz",
-            logo: "allianz.webp",
-        },
-    },
-    {
-        position: "Sales Manager",
-        company: {
-            name: "Bezmialem Vakıf Üniversitesi",
-            logo: "bezmia-lem-vak-f-u-niversitesi-logosu.png",
-        },
-    },
-];
 
 export default function Home() {
     return (
@@ -78,6 +27,16 @@ export default function Home() {
                     title="Öne çıkan iş ilanları"
                     items={[...openPositions, ...openPositions]}
                 />
+                <div className={classnames.titledImages}>
+                    <TitledImage
+                        image="/assets/employee.png"
+                        titles={["İş Ara", "Profilini Oluştur"]}
+                    />
+                    <TitledImage
+                        image="/assets/employer.png"
+                        titles={["İş Veren Çözümleri"]}
+                    />
+                </div>
             </div>
             <div id="footer"></div>
         </main>
